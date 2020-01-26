04973913229a02230460c55f23178fa21fbcc020
NoBroker <manish.kumar@nobroker.in>
2020-01-26 22:26:33 +0530

    call apply bind file created

--- callapplybind.js
+++ callapplybind.js
@@ -1,3 +1,11 @@
+
+// “this” always refers to an object.
+// “this” refers to an object which calls the function it contains.
+// In the global context “this” refers to either window object or is undefined if the ‘strict mode’ is used.
+
+
+// call, apply, bind js method examples
+
 let userObject1 = {
     firstName:'Manish',
     lastName:'Balyan'
@@ -12,11 +20,11 @@ let printFullName = function(email,phone){
     console.log(`Name: ${this.firstName} ${this.lastName} \nemail: ${email} \nphone: ${phone}`)
 }
 
-//call method     
+//call method (borrowing functions in JavaScript)   
 printFullName.call(userObject1,'balyan05.manish@gmail.com','9999873524');
 printFullName.call(userObject2,'msmayank2012@gmail.com','9999999999')
 
-// apply method
+// apply method (borrowing functions in JavaScript)
 printFullName.apply(userObject1,['balyan05.manish@gmail.com','9999873524']);
 printFullName.apply(userObject2,['msmayank2012@gmail.com','9999999999'])
 
